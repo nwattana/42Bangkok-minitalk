aa5ac2057bc70751357d8c6deff08f5d9b97067b
Narin Wattanasiri <nwattana@z3t7c4.42bangkok.com>
2022-06-19 09:43:09 +0700

    train comunication between process

--- Makefile
+++ Makefile
@@ -2,16 +2,20 @@ LIBFT_DIR = libftprintf/
 LIBFT_PRINT = libftprintf/libftprintf.a
 LIBFT_INC = libftprintf/includes
 
-RED= \e[1;31m
-GREEN= \e[1;32m
+RED=\e[1;31m
+GREEN=\e[1;32m
+YELLOW=\e[1;33m
+BLUE=\e[1;34m
+PINK=\e[1;35m
+SKYBULE=\e[1;36m
+WHITE=\e[1;37m
 
 ENDCOLOR= \e[0m
 
 
 
-
 all:
-	@printf "$(RED)%s\e[0m\e[1;32m%s\e[0m\n" "Hello " "TENT"
+	@printf "$(RED)%s\e[0m\e[1;32m%s\e[0m\n" "Hello " $(USER)
 
 main:
 	@printf "${GREEN}%s${ENDCOLOR}\n" "complie file main"
---
+++ process/process_communicate.c
@@ -0,0 +1,68 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   process_communicate.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nwattana <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/06/19 02:35:56 by nwattana          #+#    #+#             */
+/*   Updated: 2022/06/19 09:41:56 by nwattana         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+#include <signal.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <sys/wait.h>
+
+void	handler_sigusr1(int sig){
+	printf("\n(HINT)Remember that multiplication is repetitive addition\n");
+}
+
+int	main(int argc, char *argv[])
+{
+	if (argc > 0)
+	{
+		printf("Process name = \e[1;32m%s\e[0m\n", argv[0]);
+	}
+	int pid = fork();
+	if (pid == -1)
+		return (1);
+	if (pid == 0){
+		/// child Process
+		printf("pid == 0");
+		sleep(5);
+		kill(getppid(),SIGUSR1);
+	}else{
+		/*struct sigaction
+		 *{
+		 * 	union __sigaction_u 	__sigaction_u;
+		 * 	sigset_t 		sa_mask;
+		 * 	int			sa_flags;
+		 *};
+		 * 
+		 *union __sigaction_u
+		 *{
+		 *	void	(*__sig_handler)(int);
+		 *	void	(*__sig_action)(int, siginfo_t *, void *);
+		 *};
+		 *
+		 * */
+		struct sigaction sa = {0};
+		sa.sa_flags = SA_RESTART;
+		sa.sa_handler = &handler_sigusr1;
+		sigaction(SIGUSR1,&sa, NULL);
+
+		int x;
+		printf("What is the result of 3 x 5?\n");
+		scanf("%d",&x);
+		{
+			if (x==15)
+				printf("\e[1;32mRight!\e[0m\n");
+			else
+				printf("\e[1;30mWrong!\e[0m\n");
+		}
+	}
+	wait(NULL);
+}
